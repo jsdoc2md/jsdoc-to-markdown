250d00a61756ffe56f3c5d7b66798a4cb58d9694
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-03 18:58:08 +0100

    new wodge, better jsdoc template, default.hbs wip

--- jsdoc-template/publish.js
+++ jsdoc-template/publish.js
@@ -1,4 +1,5 @@
-var util = require("util");
+var util = require("util"),
+	w = require("wodge");
 
 /**
  * Generate documentation output.
@@ -9,18 +10,7 @@ var util = require("util");
  */
 exports.publish = function(data, opts) {
     var json = data({ undocumented: { "!is": true }}).map(function(record){
-        var output = {};
-        if (record.description) output.description = record.description;
-        if (record.name) output.name = record.name;
-        if (record.kind) output.kind = record.kind;
-        if (record.classdesc) output.classdesc = record.classdesc;
-        if (record.augments) output.augments = record.augments;
-        if (record.memberof) output.memberof = record.memberof;
-        if (record.scope) output.scope = record.scope;
-        if (record.params) output.params = record.params;
-        if (record.returns) output.returns = record.returns;
-        if (record.examples) output.examples = record.examples;
-        return output;
+        return w.without(record, [ "comment", "meta", "undocumented", "___id", "___s" ]);
     });
     console.log(JSON.stringify(json, null, "  "));
 };
