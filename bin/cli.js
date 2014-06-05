b29bf7dd62013e3c71fec068336a98778a058a01
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-05 15:15:03 +0100

    --preset option

--- bin/cli.js
+++ bin/cli.js
@@ -13,6 +13,7 @@ require("../")(boil);
 
 var argv = cliArgs([
     { name: "template", alias: "t", type: String },
+    { name: "preset", alias: "p", type: String },
     { name: "json", alias: "j", type: Boolean },
     { name: "src", type: Array, defaultOption: true },
 ]).parse();
@@ -22,18 +23,23 @@ if (!argv.src){
     process.exit(1);
 }
 
-var templatePath = path.resolve(__dirname, "..", "jsdoc-template"),
+var jsdocTemplatePath = path.resolve(__dirname, "..", "jsdoc-template"),
     cmd = util.format(
 		"%s -t %s %s", 
 		path.resolve(__dirname, "..", "node_modules", ".bin", "jsdoc"),
-		templatePath, 
+		jsdocTemplatePath, 
 		argv.src.join(" ")
 	);
 
 function render(data){
-    var template = argv.template
-        ? mfs.read(argv.template)
-        : mfs.read(path.resolve(__dirname, "..", "templates", "default.hbs"))
+	var template = "";
+	if (argv.template){
+		template = mfs.read(argv.template);
+	} else if (argv.preset){
+		template = mfs.read(path.resolve(__dirname, "..", "templates", argv.preset + ".hbs"));
+	} else {
+		template = mfs.read(path.resolve(__dirname, "..", "templates", "default.hbs"));
+	}
     console.log(boil.render(template, data));
 }
 
