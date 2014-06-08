2adae511c6e0c1d0dce5e17a6ecf6b4ef3b60705
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-08 18:52:33 +0100

    rest param in methodsig

--- helpers/method-sig.js
+++ helpers/method-sig.js
@@ -3,7 +3,11 @@ var w = require("wodge");
 module.exports = function(handlebars){
     handlebars.registerHelper("methodSig", function(params){
         return w.arrayify(params).map(function(param){
-            return param.optional ? "[" + param.name + "]" : param.name;
+            if (param.variable){
+                return param.optional ? "[..." + param.name + "]" : "..." + param.name;
+            } else {
+                return param.optional ? "[" + param.name + "]" : param.name;
+            }
         }).join(", ");
     });
 };
