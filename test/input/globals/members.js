3b98e7de10577d98ddfc3e39c81184725883d9cb
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-18 10:51:16 +0100

    wip

--- test/input/globals/members.js
+++ test/input/globals/members.js
@@ -9,17 +9,48 @@ var variable = 1;
 var typed = 1;
 
 /**
+@type {string|number}
+*/
+var types = 1;
+
+/**
 a var with an example
 @example
+```js
 var another = 100;
+```
 */
 var exampled = 1;
 
 /**
 a var with multiple examples
 @example
+```js
 var another = 100;
+```
 @example
+```js
 var next = "p";
+```
 */
 var examples = 1;
+
+/**
+@protected
+*/
+var _protected = true;
+
+/**
+@public
+*/
+var _public = true;
+
+/**
+@private
+*/
+var _private = true;
+
+/**
+@author Clive Jones <clive@jones.com>
+*/
+var withAuthor = true;
--- test/output/globals/members.md
+++ test/output/globals/members.md
@@ -2,18 +2,32 @@
 a global variable
 
 ###typed :string
+###types :string|number
 ###exampled
 a var with an example
 
 ####Example
+```js
 var another = 100;
+```
 
 ###examples
 a var with multiple examples
 
 ####Example
+```js
 var another = 100;
+```
 
 ####Example
+```js
 var next = "p";
+```
 
+###_protected
+Access: protected  
+###_public
+###_private
+Access: private  
+###withAuthor
+Author: Clive Jones <clive@jones.com>  
