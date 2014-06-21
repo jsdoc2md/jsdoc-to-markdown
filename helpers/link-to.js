a2a506ca5945a51636cba1dfba49c78059e9ef64
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-21 16:46:30 +0100

    linkTo fix

--- helpers/link-to.js
+++ helpers/link-to.js
@@ -2,16 +2,17 @@ var a = require("array-tools"),
     util = require("util");
 
 module.exports = function(handlebars){
-    handlebars.registerHelper("linkTo", function linkTo(longname){
+    handlebars.registerHelper("linkTo", function linkTo(longname, options){
         if (Array.isArray(longname)){
             return longname.map(function(name){
-                return linkTo(name);
+                return linkTo(name, options);
             });
         } else {
             if (["string", "object", "number", "boolean", "array"].indexOf(longname) > -1){
                 return "`" + longname + "`";
             } else {
-                return util.format("[%s](#%s)", longname, handlebars.helpers.anchorName(longname));
+                var linked = a.findWhere(options.data.root, { longname: longname });
+                return util.format("[%s](#%s)", linked.name || "!error!",handlebars.helpers.anchorName(longname));
             }
         }
     });
