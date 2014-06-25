8e6c4ca8c4dd3e30695be58261746c897a8053e5
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-25 14:32:02 +0100

    link-to

--- helpers/link-to.js
+++ helpers/link-to.js
@@ -9,19 +9,21 @@ module.exports = function(handlebars){
                 return linkTo(name, options);
             });
         } else {
+            if (re.test(longname)){
+                var fullName = longname;
+                fullName = fullName.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+                longname = longname.match(re)[1];
+            }
+            
             var builtInType = /string|object|number|boolean|array/i.test(longname);
+            
             if (builtInType){
-                return "`" + longname + "`";
+                return "`" + (fullName || longname) + "`";
             } else {
-                if (re.test(longname) && !builtInType){
-                    var fullName = longname;
-                    fullName = fullName.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-                    longname = longname.match(re)[1];
-                }
                 var linked = a.findWhere(options.data.root, { longname: longname });
                 return linked 
                     ? util.format("[%s](#%s)", fullName ? fullName.replace(longname, linked.name) : linked.name, handlebars.helpers.anchorName(longname))
-                    : "`" + longname + "`";
+                    : "`" + (fullName || longname) + "`";
             }
         }
     });
