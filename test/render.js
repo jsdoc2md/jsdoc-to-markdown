865f25d404fb563f463ce6b946c9118d3d8c89be
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-25 16:39:32 +0100

    default preset

--- test/render.js
+++ test/render.js
@@ -5,7 +5,7 @@ var yuidoc2md = require("../"),
     mfs = require("more-fs");
 
 function halt(err){
-    console.error("Error: " + err.message);
+    console.error(err.stack || "Error: " + err.message);
     process.exit(1);
 }
 
@@ -19,19 +19,24 @@ function render(input, output, preset, index){
 fs.readdirSync("test/input/commonjs").forEach(function(file){
     render(
         path.join("test/input/commonjs", file), 
-        path.join("test/output/commonjs", path.basename(file, ".js") + ".md"), 
-        "modules"
+        path.join("test/output/commonjs", path.basename(file, ".js") + ".md")
+    );
+});
+
+fs.readdirSync("test/input/amd").forEach(function(file){
+    render(
+        path.join("test/input/amd", file), 
+        path.join("test/output/amd", path.basename(file, ".js") + ".md")
     );
 });
 
 fs.readdirSync("test/input/globals").forEach(function(file){
     render(
         path.join("test/input/globals", file), 
-        path.join("test/output/globals", path.basename(file, ".js") + ".md"), 
-        "globals"
+        path.join("test/output/globals", path.basename(file, ".js") + ".md")
     );
 });
 
-render("test/input/globals/*.js", "test/output/globals/all.md", "globals");
+render("test/input/globals/*.js", "test/output/globals/all.md");
 render("test/input/commonjs/object.js", "test/output/commonjs/object-with-index.md", "modules", true);
 render("test/input/commonjs/file-set.js", "test/output/commonjs/fileset-with-index.md", "modules", true);
