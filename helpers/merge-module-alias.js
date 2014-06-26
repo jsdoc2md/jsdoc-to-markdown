e4c3d86f08918afc6d74f22387cfbb26968447e0
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-26 09:35:00 +0100

    license, copyright etc

--- helpers/merge-module-alias.js
+++ helpers/merge-module-alias.js
@@ -5,7 +5,7 @@ module.exports = function(handlebars){
     handlebars.registerHelper("mergeModuleAlias", function(options){
         var alias = a.findWhere(options.data.root, { alias: "module:" + this.name });
         if (alias){
-            this.description = this.description || alias.description;
+            this.description = this.description || alias.classdesc || alias.description;
             this.params = this.params || alias.params;
             this.returns = this.returns || alias.returns;
             this.examples = this.examples || alias.examples;
