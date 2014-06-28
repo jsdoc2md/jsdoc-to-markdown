9c2f2257e41462168da36cc40b561906c2272a12
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-28 17:25:04 +0100

    index

---
+++ test/output/all-with-index.js
@@ -0,0 +1,42 @@
+* module: amd/file-set
+* module: jacket
+  * function: zip <sub>instance</sub>
+  * function: zip <sub>instance</sub>
+* module: my/shirt
+  * member: color <sub>static</sub>
+  * class: Turtleneck <sub>static</sub>
+* module: my/ShirtCreator
+* module: commonjs/file-set
+* module: commonjs/function
+* module: commonjs/ignore
+  * member: visible <sub>static</sub>
+  * member: invisible <sub>static</sub>
+* module: jacket
+  * function: zip <sub>instance</sub>
+  * function: zip <sub>instance</sub>
+* module: commonjs/object-with-alias
+  * member: one <sub>static</sub>
+  * member: two <sub>static</sub>
+  * function: three <sub>static</sub>
+* module: cyberdyne
+  * function: createMachine <sub>static</sub>
+  * class: Machine <sub>inner</sub>
+  * member: something <sub>inner</sub>
+* module: cattle
+  * function: createCow <sub>static</sub>
+  * member: innerMember <sub>inner</sub>
+  * function: innerFuction <sub>inner</sub>
+* module: heaven
+  * function: createCloud <sub>static</sub>
+  * member: Cloud <sub>static</sub>
+  * class: Cloud <sub>static</sub>
+* module: commonjs/object
+  * member: one <sub>static</sub>
+  * member: two <sub>static</sub>
+  * function: three <sub>static</sub>
+* module: commonjs/private
+  * member: notprivate <sub>static</sub>
+  * member: isprivate <sub>static</sub>
+* module: commonjs/single-value
+* module: commonjs/sum-alias
+* module: commonjs/sum
