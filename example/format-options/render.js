1fd3f511fcc44cdd7e1897676d463034f78acbfb
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-02-01 15:46:00 +0000

    re-org

--- example/format-options/render.js
+++ example/format-options/render.js
@@ -5,40 +5,40 @@ var fs = require("fs");
 /* global-index-format */
 jsdoc2md
     .render("example/src/*.js", { template: "example/format-options/template/global-index.hbs"})
-    .pipe(fs.createWriteStream("example/format-options/md/global-index-format.md"));
+    .pipe(fs.createWriteStream("example/format-options/global-index-format.md"));
 
 /* module-index-format */
 jsdoc2md
     .render("example/src/commonJS/*.js", { template: "example/format-options/template/module-index.hbs"})
-    .pipe(fs.createWriteStream("example/format-options/md/module-index-format.md"));
+    .pipe(fs.createWriteStream("example/format-options/module-index-format.md"));
 
 /* param-list-format */
 jsdoc2md
     .render("example/src/function.js", { template: "example/format-options/template/param-list-format.hbs"})
-    .pipe(fs.createWriteStream("example/format-options/md/param-list-format.md"));
+    .pipe(fs.createWriteStream("example/format-options/param-list-format.md"));
 
 /* property-list-format */
 jsdoc2md
     .render("example/src/enum.js", { template: "example/format-options/template/property-list-format.hbs"})
-    .pipe(fs.createWriteStream("example/format-options/md/property-list-format.md"));
+    .pipe(fs.createWriteStream("example/format-options/property-list-format.md"));
 
 /* class-member-index */
 jsdoc2md
     .render("example/src/*.js", { template: "example/format-options/template/class-member-index.hbs"})
-    .pipe(fs.createWriteStream("example/format-options/md/class-member-index.md"));
+    .pipe(fs.createWriteStream("example/format-options/class-member-index.md"));
 
 /* separators */
 jsdoc2md
     .render("example/src/commonJS/*.js", { separators: true })
-    .pipe(fs.createWriteStream("example/format-options/md/separators.md"));
+    .pipe(fs.createWriteStream("example/format-options/separators.md"));
 
 /* name-format */
 jsdoc2md
     .render("example/src/commonJS/*.js", { "name-format": true, template: "example/format-options/template/name-format.hbs" })
-    .pipe(fs.createWriteStream("example/format-options/md/name-format.md"));
+    .pipe(fs.createWriteStream("example/format-options/name-format.md"));
 
 /* no-gfm */
 jsdoc2md
     .render(["example/src/function.js", "example/src/enum.js"], { template: "example/format-options/template/no-gfm.hbs" })
-    .pipe(fs.createWriteStream("example/format-options/md/no-gfm.md"));
+    .pipe(fs.createWriteStream("example/format-options/no-gfm.md"));
 
--- example/identifier-blocks/render.js
+++ example/identifier-blocks/render.js
@@ -5,4 +5,4 @@ var fs = require("fs");
 /* class */
 jsdoc2md
     .render("example/src/*.js", { template: "example/identifier-blocks/template/class.hbs"})
-    .pipe(fs.createWriteStream("example/identifier-blocks/md/class.md"));
+    .pipe(fs.createWriteStream("example/identifier-blocks/class.md"));
