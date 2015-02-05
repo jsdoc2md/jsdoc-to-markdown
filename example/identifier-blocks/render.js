01a7ffd79e181c27c307938d8a51a03d11af6c03
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-02-05 11:09:49 +0000

    {{#modules}} example

--- example/identifier-blocks/render.js
+++ example/identifier-blocks/render.js
@@ -4,5 +4,11 @@ var fs = require("fs");
 
 /* class */
 jsdoc2md
-    .render("example/src/*.js", { template: "example/identifier-blocks/template/class.hbs"})
-    .pipe(fs.createWriteStream("example/identifier-blocks/class.md"));
+    .render("example/src/*.js", { template: "example/identifier-blocks/template/single-class.hbs"})
+    .pipe(fs.createWriteStream("example/identifier-blocks/single-class.md"));
+
+/* modules */
+jsdoc2md
+    .render("example/src/commonJS/*.js", { template: "example/identifier-blocks/template/modules.hbs"})
+    .pipe(fs.createWriteStream("example/identifier-blocks/modules.md"));
+
