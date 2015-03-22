ecba875ee981962ab534ed5a6e2f769ea6ac7097
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-03-22 17:34:00 +0000

    examples - return

--- example/tags/return/render.js
+++ example/tags/return/render.js
@@ -3,12 +3,18 @@ var jsdoc2md = require("../../../");
 var fs = require("fs");
 
 var p = {
-    input: __dirname + "/../../src/*.js",
+    input: __dirname + "/src.js",
     template: __dirname + "/template.hbs",
     output: __dirname + "/output.md"
 };
 
+var source = fs.readFileSync(p.input, "utf8");
+
 jsdoc2md
-    .render(p.input, { template: p.template })
+    .render(p.input, {
+        "global-index-format": "none",
+        template: p.template,
+        source: source
+    })
     .on("error", console.error)
     .pipe(fs.createWriteStream(p.output));
