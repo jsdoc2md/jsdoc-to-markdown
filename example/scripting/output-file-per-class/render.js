c5ed44a78886ec53c97ba4f8c3aafdf7b2ba3fec
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-11-12 16:14:46 +0000

    simplify 'output-file-per-class' script

--- example/scripting/output-file-per-class/render.js
+++ example/scripting/output-file-per-class/render.js
@@ -4,48 +4,42 @@ var fs = require('fs')
 var dmd = require('dmd')
 var util = require('util')
 var path = require('path')
+var collectJson = require('collect-json')
 
 /* paths used by this script */
 var p = {
   src: path.resolve(__dirname, '../../src/*.js'),
-  json: path.resolve(__dirname, './source.json'),
   output: path.resolve(__dirname, './%s.md')
 }
 
-/* we only need to parse the source code once, so cache it */
 jsdoc2md({ src: p.src, json: true })
-  .pipe(fs.createWriteStream(p.json))
-  .on('close', dataReady)
+  .pipe(collectJson(function (data) {
 
-function dataReady () {
-  /* parse the jsdoc-parse output.. */
-  var data = require(p.json)
+    /* reduce the jsdoc-parse output to an array of class names */
+    var classes = data.reduce(function (prev, curr) {
+      if (curr.kind === 'class') prev.push(curr.name)
+      return prev
+    }, [])
 
-  /* ..because we want an array of class names */
-  var classes = data.reduce(function (prev, curr) {
-    if (curr.kind === 'class') prev.push(curr.name)
-    return prev
-  }, [])
+    /* render an output file for each class */
+    writeMarkdownFile(data, classes, 0)
+  }))
 
-  /* render an output file for each class */
-  renderMarkdown(classes, 0)
-}
-
-function renderMarkdown (classes, index) {
+function writeMarkdownFile (data, classes, index) {
   var className = classes[index]
   var template = util.format('{{#class name="%s"}}{{>docs}}{{/class}}', className)
   console.log(util.format(
     'rendering %s, template: %s', className, template
   ))
-  return fs.createReadStream(p.json)
-    .pipe(dmd({ template: template }))
+
+  var dmdStream = dmd({ template: template })
+  dmdStream
     .pipe(fs.createWriteStream(util.format(p.output, className)))
     .on('close', function () {
       var next = index + 1
       if (classes[next]) {
-        renderMarkdown(classes, next)
-      } else {
-        fs.unlinkSync(p.json)
+        writeMarkdownFile(data, classes, next)
       }
     })
+  dmdStream.end(JSON.stringify(data))
 }
