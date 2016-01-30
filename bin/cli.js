9c3584287d4e22a10bf343ed8cb63d8d3d3dcd92
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-01-30 23:14:09 +0000

    cli refactor

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,8 @@
 #!/usr/bin/env node
 'use strict'
-var fs = require('fs')
 var commandLineArgs = require('command-line-args')
-var jsdoc2md = require('../')
-var loadConfig = require('config-master')
-var o = require('object-tools')
-var cliData = require('../lib/cli-data')
 var tool = require('command-line-tool')
+var cliData = require('../lib/cli-data')
 
 var cli = commandLineArgs(cliData.definitions)
 var usage = cli.getUsage(cliData.usage)
@@ -17,14 +13,23 @@ try {
   tool.stop(1, { message: err })
 }
 
-var dmdConfig = loadConfig('dmd')
-var parseConfig = loadConfig('jsdoc-parse')
-var jsdoc2mdConfig = loadConfig('jsdoc2md')
+if (argv.help) {
+  tool.stop(0, { usage: usage })
+} else {
+  var fs = require('fs')
+  var jsdoc2md = require('../')
+  var loadConfig = require('config-master')
+  var o = require('object-tools')
 
-var config = o.extend(parseConfig, dmdConfig, jsdoc2mdConfig, argv._all)
+  var dmdConfig = loadConfig('dmd')
+  var parseConfig = loadConfig('jsdoc-parse')
+  var jsdoc2mdConfig = loadConfig('jsdoc2md')
 
-if (config.template) config.template = fs.readFileSync(config.template, 'utf8')
-if (config.help) tool.stop(0, { usage: usage })
-if (config.config) tool.stop(0, { message: JSON.stringify(o.without(config, 'config'), null, '  ') })
+  var config = o.extend(parseConfig, dmdConfig, jsdoc2mdConfig, argv._all)
 
-jsdoc2md(config).pipe(process.stdout)
+  if (config.template) config.template = fs.readFileSync(config.template, 'utf8')
+  if (config.help) tool.stop(0, { usage: usage })
+  if (config.config) tool.stop(0, { message: JSON.stringify(o.without(config, 'config'), null, '  ') })
+
+  jsdoc2md(config).pipe(process.stdout)
+}
