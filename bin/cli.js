d6acc1ede695c3d4663bde52653d56cd40c16ab3
Lloyd Brookes <lloyd.brookes@gmail.com>
2016-04-28 22:06:55 +0100

    fixed issue where --help usage was only partially displayed on node v6

--- bin/cli.js
+++ bin/cli.js
@@ -9,45 +9,6 @@ var o = require('object-tools')
 var cliData = require('../lib/cli-data')
 var ansi = require('ansi-escape-sequences')
 
-var cli = commandLineArgs(cliData.definitions)
-var usage = cli.getUsage(cliData.usage)
-
-try {
-  var argv = cli.parse()
-} catch (err) {
-  halt(err)
-}
-
-var dmdConfig = loadConfig('dmd')
-var parseConfig = loadConfig('jsdoc-parse')
-var jsdoc2mdConfig = loadConfig('jsdoc2md')
-
-var config = o.extend(parseConfig, dmdConfig, jsdoc2mdConfig, argv._all)
-
-if (config.template) {
-  config.template = fs.readFileSync(config.template, 'utf8')
-}
-
-if (config.help) {
-  console.log(usage)
-  process.exit(0)
-}
-
-if (config.config) {
-  console.log(JSON.stringify(o.without(config, 'config'), null, '  '))
-  process.exit(0)
-}
-
-var d = domain.create()
-d.on('error', halt)
-d.run(function () {
-  if (config.src) {
-    jsdoc2md(config).pipe(process.stdout)
-  } else {
-    process.stdin.pipe(jsdoc2md(config)).pipe(process.stdout)
-  }
-})
-
 function halt (err) {
   if (err.code === 'EPIPE') process.exit(0) /* no big deal */
 
@@ -62,5 +23,41 @@ function halt (err) {
     console.error(ansi.format(err.message, 'red'))
   }
   console.error(usage)
-  process.exit(1)
+  process.exitCode = 1
+}
+
+var cli = commandLineArgs(cliData.definitions)
+var usage = cli.getUsage(cliData.usage)
+
+try {
+  var argv = cli.parse()
+} catch (err) {
+  halt(err)
+  return
+}
+
+var dmdConfig = loadConfig('dmd')
+var parseConfig = loadConfig('jsdoc-parse')
+var jsdoc2mdConfig = loadConfig('jsdoc2md')
+
+var config = o.extend(parseConfig, dmdConfig, jsdoc2mdConfig, argv._all)
+
+if (config.template) {
+  config.template = fs.readFileSync(config.template, 'utf8')
+}
+
+if (config.help) {
+  console.log(usage)
+} else if (config.config) {
+  console.log(JSON.stringify(o.without(config, 'config'), null, '  '))
+} else {
+  var d = domain.create()
+  d.on('error', halt)
+  d.run(function () {
+    if (config.src) {
+      jsdoc2md(config).pipe(process.stdout)
+    } else {
+      process.stdin.pipe(jsdoc2md(config)).pipe(process.stdout)
+    }
+  })
 }
