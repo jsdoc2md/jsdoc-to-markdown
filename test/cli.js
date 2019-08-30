1346934e1127a9c2bf1251289ee14f83755d1954
Lloyd Brookes <lloyd.brookes@gmail.com>
2019-08-30 23:04:17 +0100

    upgrade deps

--- test/cli.js
+++ test/cli.js
@@ -17,8 +17,8 @@ tom.test('cli: json option', function () {
   return new Promise((resolve, reject) => {
     const handle = spawn(
       'node',
-      [ 'bin/cli.js', '--no-usage-stats', '--no-cache', '--json', inputPath ],
-      { stdio: [ 'ignore', outputFile, 'ignore' ] }
+      ['bin/cli.js', '--no-usage-stats', '--no-cache', '--json', inputPath],
+      { stdio: ['ignore', outputFile, 'ignore'] }
     )
     handle.on('close', function () {
       const json = fs.readFileSync('tmp-test/ignore.json', 'utf8')
